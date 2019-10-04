d36ca506ea52a99a6c98a876fdbfed619c800b2a
MirkoPieri <mirko97.pieri@gmail.com>
2019-10-04 12:05:39 +0200

    Aggiunto style con css.

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -7,13 +7,17 @@
     </div>
     @foreach ($books as $value)
       <div class="box">
-        <span><p>Name:</p> {{$value -> name}}</span>
-        <span><p>Description:</p> {{$value -> description}}</span>
-        <span><p>Author:</p> {{$value -> author}}</span>
+        <div class="text">
+          <span><p>Name:</p> {{$value -> name}}</span>
+          <span><p>Description:</p> {{$value -> description}}</span>
+          <span><p>Author:</p> {{$value -> author}}</span>
+        </div>
 
         <div class="haref">
-          <a href="{{ route('edit.category', $value -> id) }}">EDIT</a>
-          <a href="{{ route('destroy.category', $value -> id) }}">DELETE</a>
+          <div class="link">
+            <a href="{{ route('edit.category', $value -> id) }}"><img src="image/edit.png" alt="edit"></a>
+            <a href="{{ route('destroy.category', $value -> id) }}"><img src="image/delete.png" alt="delete"></a>
+          </div>
         </div>
       </div>
     @endforeach
